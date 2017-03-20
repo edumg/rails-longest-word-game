1224be199e5c3a44c0be8d5f489c7d0bdd9646f7
eduardo gregorio <edumgregorio@gmail.com>
2017-03-20 17:19:21 +0100

    Rails-Longest-word-game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'game', to: 'longestword#game'
+
+  get 'score', to: 'longestword#score'
+
 end
+
+
